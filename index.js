5976f18368e4068e1f95ddb7808706bc6427b3ff
JatinS <jatin@JatinS>
2021-07-02 21:54:13 +0530

    debounced

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-console.log('hey');
+// chokidar is the node package that will be used to fire events on changes in specified dir
+// please read the documentation
+
+const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+
+const addCallback = debounce(() => {
+	console.log('files added');
+}, 100);
+
+const log = console.log.bind(console);
+const cwd = chokidar.watch('.');
+cwd
+	.on('add', addCallback)
+	.on('change', (path) => log(`${path} is changed`))
+	.on('unlink', (path) => log(`${path} is unlinked`));
